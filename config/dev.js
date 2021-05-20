e372f65b2849215447a35137bf7d3cd7c4d4ef23
Pradhyumna Singh rathore <pradhyumnarathore@gmail.com>
2021-05-20 10:48:55 +0530

    added

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports=
     {
         MongoDB:process.env.MONGODB,
-        AWSAccessID:'AKIA6IDGMDIZDFS6CLOE',
-        AWSSecretKey:'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'
+        AWSAccessID:process.env.AWSACCESSID,
+        AWSSecretKey:process.env.AWSSECRETKEY
 
     }
